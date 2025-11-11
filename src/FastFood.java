41c6df2ddcd300c30c24a54e9d332fa3ba26a0f6
Iordache Andrei <ai401@cti.ugal.ro>
2025-11-11 08:51:25 +0200

    Clasa LocalCulinar finalizata!

--- src/FastFood.java
+++ src/FastFood.java
@@ -1,2 +1,37 @@
-public class FastFood {
+public class FastFood extends LocalCulinar {
+    private String tipBucatarie;
+    private int numarProduse;
+    private String[] listaSandwichuri;
+
+    // Constructor fără parametri
+    public FastFood() {
+        super(); // apelează constructorul fără parametri din LocalCulinar
+        this.tipBucatarie = "Internațional";
+        this.numarProduse = 0;
+        this.listaSandwichuri = new String[0];
+    }
+
+    // Constructor cu parametri
+    public FastFood(String denumire, String locatie, double rating,
+                    String tipBucatarie, int numarProduse, String[] listaSandwichuri) {
+        super(denumire, locatie, rating); // apelează constructorul cu parametri din LocalCulinar
+        this.tipBucatarie = tipBucatarie;
+        this.numarProduse = numarProduse;
+        this.listaSandwichuri = listaSandwichuri;
+    }
+
+    // Constructor de copiere
+    public FastFood(FastFood other) {
+        super(other); // copiază atributele din clasa părinte
+        this.tipBucatarie = other.tipBucatarie;
+        this.numarProduse = other.numarProduse;
+        this.listaSandwichuri = other.listaSandwichuri.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                " | Tip bucătărie: " + tipBucatarie +
+                " | Număr produse: " + numarProduse;
+    }
 }
--- src/LocalCulinar.java
+++ src/LocalCulinar.java
@@ -1,6 +1,60 @@
 public abstract class LocalCulinar {
     private String denumire;
     private String locatie;
-    private Float rating;
+    private double rating;
 
+    // 🔸 Constructor fără parametri
+    public LocalCulinar() {
+        this.denumire = "Necunoscut";
+        this.locatie = "Necunoscută";
+        this.rating = 0.0;
+    }
+
+    // 🔸 Constructor cu toți parametrii
+    public LocalCulinar(String denumire, String locatie, double rating) {
+        this.denumire = denumire;
+        this.locatie = locatie;
+        this.rating = rating;
+    }
+
+    // 🔸 Constructor de copiere
+    public LocalCulinar(LocalCulinar other) {
+        this.denumire = other.denumire;
+        this.locatie = other.locatie;
+        this.rating = other.rating;
+    }
+
+    // 🔸 Getteri
+    public String getDenumire() {
+        return denumire;
+    }
+
+    public String getLocatie() {
+        return locatie;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    // 🔸 Setteri
+    public void setDenumire(String denumire) {
+        this.denumire = denumire;
+    }
+
+    public void setLocatie(String locatie) {
+        this.locatie = locatie;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    // 🔸 Metoda toString
+    @Override
+    public String toString() {
+        return "Local: " + denumire +
+                " | Locație: " + locatie +
+                " | Rating: " + rating;
+    }
 }
